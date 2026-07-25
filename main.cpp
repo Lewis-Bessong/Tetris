4d131d181f6d7bd5bcd3be704c58c7b91183046d
Lewis Bessong <lewistkbessong@gmail.com>
2026-07-24 19:49:56 -0500

    Added an asset towards game production. (wstring  array of 7 and assigning visual representations within each box

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+wstring tetromino[7]; // game space (array of 7 (independent wstring that can be assigned) )
+
+
+int main()
+{
+    //create assets (things game will use as its playing)
+
+    // shapes/tetris block shapes 
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L"..X.");
+
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L".XX.");
+    tetromino[2].append(L".X..");
+    tetromino[2].append(L".X..");
+
+    tetromino[3].append(L".X..");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L"..X.");
+    tetromino[3].append(L"..X.");
+    
+
+
+
+    return 0;
+}
